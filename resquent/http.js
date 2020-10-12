0c866c29ded6881af3e15e01ea7a4e2346ad3f69
lifazhan <lifazhan233@gmail.com>
2020-10-12 20:53:08 +0800

    主页分页请求数据

--- resquent/http.js
+++ resquent/http.js
@@ -5,6 +5,8 @@ axios.defaults.adapter = mpAdapter
 // 构建axios实例
 const http = axios.create({
   baseURL: 'http://localhost:3000/api'
+  /* baseURL: 'http://243769da24.wicp.vip/api' */
+
 })
 
 // 到处实例
