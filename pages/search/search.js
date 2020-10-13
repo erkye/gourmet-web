3b238cc3451579f17d02347fa945ac1fa1ab63d4
lifazhan <lifazhan233@gmail.com>
2020-10-13 20:55:02 +0800

    首页优化&搜索页面开始

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,11 +1,19 @@
 // pages/search/search.js
+// 引用封装的请求接口
+import http from '../../resquent/http'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 用户输入的搜索内容
+    searchValue: ''
+  },
+  // 用户点击搜索按钮
+  async tapSearch(){
+    console.log(this.data.searchValue);
   },
 
   /**
@@ -15,52 +23,5 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
--- resquent/http.js
+++ resquent/http.js
@@ -4,7 +4,11 @@ axios.defaults.adapter = mpAdapter
 
 // 构建axios实例
 const http = axios.create({
+  /* 本地测试环境使用 */
   baseURL: 'http://localhost:3000/api'
+  /* 真机调试使用 */
+  /* baseURL: 'http://243769da24.wicp.vip/api' */
+  /* 生产环境使用 */
   /* baseURL: 'http://243769da24.wicp.vip/api' */
 
 })
