4416488e12770435a489de7ee2432b60068e5c34
lifazhan <lifazhan233@gmail.com>
2020-10-14 21:19:49 +0800

    搜索页面基本完成&发布页面添加富文本编辑器

--- pages/search/search.js
+++ pages/search/search.js
@@ -9,11 +9,36 @@ Page({
    */
   data: {
     // 用户输入的搜索内容
-    searchValue: ''
+    searchValue: '',
+    pageNo: 1,
+    pageSize: 10,
+    //是否还有下一页
+    hasNextPage: true,
+    // 查询到的数组
+    menuList: []
   },
   // 用户点击搜索按钮
   async tapSearch(){
-    console.log(this.data.searchValue);
+    //console.log(this.data.searchValue);
+    // 查询参数
+    const params = {
+      'key' : this.data.searchValue,
+      'pageNo': this.data.pageNo,
+      'pageSize': this.data.pageSize
+    }
+
+    const {data:response} = await http.get('/index/search',{params:params})
+    console.log(response);
+
+    if(response.code === 1000){
+      this.setData({
+        // 追加数据
+        menuList: [...this.data.menuList,...response.data.list],
+        // 修改是否还有下一页
+        hasNextPage: response.data.hasNextPage
+      })
+    }
+
   },
 
   /**
@@ -22,6 +47,22 @@ Page({
   onLoad: function (options) {
 
   },
+  // 页面触底函数
+  onReachBottom:function() {
+    if(this.data.hasNextPage){
+      this.setData({
+        // 页数+1
+        pageNo: this.data.pageNo+1
+      })
+      // 发起请求
+      this.tapSearch()
+    }else{
+      wx.showToast({
+        title: '已经到底了' ,
+        icon: 'none'  
+      });
+    }
+  }
 
   
 })
