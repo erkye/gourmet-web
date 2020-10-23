67e5229087230093a59da7461f4e101406b0cd7a
lifazhan <lifazhan233@gmail.com>
2020-10-23 20:47:13 +0800

    发布实现&首页下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,86 +1,106 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 // 引用封装的请求接口
-import {http} from '../../resquent/http'
+import { http } from "../../resquent/http";
 
 Page({
   data: {
     // 滑动视图的数据
-   swiperData:[],
-   // 最新佳作的数据
-   latestMenu:[],
-   //获取最新佳作时需要向后台传递的参数
-   latestParams: {
-     // 当前页码
-     pageNo: 1,
-     // 每页大小
-     pageSize: 10,
-     //是否还有下一页
-     hasNextPage: true
-   }
+    swiperData: [],
+    // 最新佳作的数据
+    latestMenu: [],
+    //获取最新佳作时需要向后台传递的参数
+    latestParams: {
+      // 当前页码
+      pageNo: 1,
+      // 每页大小
+      pageSize: 10,
+      //是否还有下一页
+      hasNextPage: true,
+    },
   },
 
   // 获取滑动视图的数据
-  async getSwipperData(){
+  async getSwipperData() {
     // 从后端获取数据
-    const {data:response} = await http.get('/index/recommend')
+    const { data: response } = await http.get("/index/recommend");
     //console.log(response)
     // 获取数据成功
-    if(response.code === 1000){
+    if (response.code === 1000) {
       // 设置滑动视图的数据
-      this.setData({swiperData:response.data})
+      this.setData({ swiperData: response.data });
     }
   },
 
   // 分页获取
-  async getLatestMenuData(){
+  async getLatestMenuData() {
     // 后端请求数据 get传参方式如下：
-    const {data:response} = await http.get('/index/latest',{params:this.data.latestParams})
+    const { data: response } = await http.get("/index/latest", {
+      params: this.data.latestParams,
+    });
     // console.log(response);
-    if(response.code === 1000){
+    if (response.code === 1000) {
       // 追加最新佳作的数据
       // ...是展开符号，可以数组或者对象展开
+
       this.setData({
         // 这里使用... 将原数组和新获得的数组数据展开，创建为一个新的数组
-        latestMenu: [...this.data.latestMenu,...response.data.list],
+        latestMenu: [...this.data.latestMenu, ...response.data.list],
         // 使用setData赋值一个对象时，需要将其所有的属性都重新写一遍，否则对象会被覆盖为只有指定属性的对象
-        latestParams:{
+        latestParams: {
           // 将原对象展开
           ...this.data.latestParams,
           // 修改原对象的某一属性
-          hasNextPage: response.data.hasNextPage
-        }
-      })
+          hasNextPage: response.data.hasNextPage,
+        },
+      });
     }
-    console.log(this.data.latestMenu)
+    console.log(this.data.latestMenu);
   },
-  
+  /* 下拉加载 */
+  onPullDownRefresh() {
+    /* 显示加载图标 */
+    wx.showNavigationBarLoading(); 
+    /* 先将原来数据清空 */
+    this.setData({
+      swiperData:[],
+      latestMenu:[]
+    })
+    // 获取滑动视图的数据
+    this.getSwipperData();
+    // 获取本周佳作的数据
+    this.getLatestMenuData();
+    // 隐藏加载图标
+    wx.stopPullDownRefresh();
+
+  },
+
   onLoad: function () {
     // 获取滑动视图的数据
-    this.getSwipperData()
+    this.getSwipperData();
     // 获取本周佳作的数据
-    this.getLatestMenuData()
+    this.getLatestMenuData();
   },
   // 页面触底函数
-  onReachBottom:function() {
-    if(this.data.latestParams.hasNextPage){
+  onReachBottom: function () {
+    if (this.data.latestParams.hasNextPage) {
       this.setData({
-        latestParams:{
+        latestParams: {
           // 将原对象展开
           ...this.data.latestParams,
           // 设置请求页面+1
-          pageNo: this.data.latestParams.pageNo+1
-        }
-      })
+          pageNo: this.data.latestParams.pageNo + 1,
+        },
+      });
       // 发起请求
       this.getLatestMenuData();
-    }else{
+    } else {
       wx.showToast({
-        title: '已经到底了' ,
-        icon: 'none'  
+        title: "已经到底了",
+        icon: "none",
       });
     }
-  }
-})
+  },
+});
--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,224 +1,348 @@
-import {http,serverIp} from '../../resquent/http'
+import { http, serverIp } from "../../resquent/http";
 
 Page({
   data: {
-
     // 标题
-    title: '',
+    title: "",
     // 简介
-    introd:'',
+    introd: "",
+    // 封面
+    img: "",
+    // 用户昵称
+    nickName: '',
     /* 材料部分需要的数据 */
     /* 材料列表 */
     materialsList: [
       {
-        name: '猪肉',
-        quantity: '1斤'
-      }
+        name: "猪肉",
+        quantity: "1斤",
+      },
     ],
 
     /* 下面时富文本编辑器需要的数据 */
     formats: {},
     readOnly: false,
-    placeholder: '请输入菜谱的步骤~',
+    placeholder: "请输入菜谱的步骤~",
     editorHeight: 800,
     keyboardHeight: 0,
-    isIOS: false
-  },  
+    isIOS: false,
+  },
+  /* 用户点击选择封面 */
+  handleSelectImage() {
+    const that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ["original", "compressed"],
+      sourceType: ["album", "camera"],
+      success: function (res) {
+        /* 上传图片到服务器 */
+        wx.uploadFile({
+          url: serverIp + "/img/upload",
+          filePath: res.tempFilePaths[0],
+          name: "pic",
+          formData: {},
+          success: (result) => {
+            const data = JSON.parse(result.data);
+            /* 成功上传 */
+            if (data.code === 1000) {
+              /* 获取该图片的url */
+              const url = data.data;
+              // 设置封面图片的url
+              that.setData({
+                img: url,
+              });
+            }
+          },
+          fail: () => {
+            wx.showToast({
+              title: "图片上传失败",
+              icon: "none",
+              duration: 1000,
+            });
+          },
+          complete: () => {},
+        });
+      },
+    });
+    /* wx.showActionSheet({
+      itemList: ['相机', '选择图片'],
+      success (res) {
+        console.log(res.tapIndex)
+      },
+      fail (res) {
+        console.log(res.errMsg)
+      }
+    }) */
+  },
   /* 点击菜谱材料中的添加按钮处理的事件 */
-  handleMaterialsAdd(){
+  handleMaterialsAdd() {
     /* 创建一个空的材料的对象 */
     const newItem = {
-      name: '',
-      quantity: ''
-    }
+      name: "",
+      quantity: "",
+    };
     /* 获取data中材料的列表 */
-    let newList =  this.data.materialsList
+    let newList = this.data.materialsList;
     /* 将新的空列表添加进去 */
-    newList.push(newItem)
+    newList.push(newItem);
     /* 将data中的材料列表数组的值更新为新的添加空元素的值 */
     this.setData({
-      materialsList: newList
-    })
+      materialsList: newList,
+    });
   },
   /* 点击菜谱材料中每一项的删除按钮的处理事件 */
-  handleMaterialsDel(e){
+  handleMaterialsDel(e) {
     /* 获取点击的索引 */
-    const index = e.currentTarget.dataset.index
+    const index = e.currentTarget.dataset.index;
     /* 获取data中的材料列表数组 */
-    let newList = this.data.materialsList
+    let newList = this.data.materialsList;
     /* 删除点击索引位置的数据 */
-    newList.splice(index,1)
+    newList.splice(index, 1);
     /* 更新数据 */
     this.setData({
-      materialsList: newList
-    })
+      materialsList: newList,
+    });
   },
   /* 材料名称输入框失去焦点时触发事件 */
-  handleUpdateMaterialsName(e){
+  handleUpdateMaterialsName(e) {
     /* 获取输入框中的值 */
-    const name = e.detail.value
+    const name = e.detail.value;
     /* 获取组件中携带的数据索引 */
-    const index = e.currentTarget.dataset.index
+    const index = e.currentTarget.dataset.index;
     /* 获取data中的材料列表数组 */
-    let newList = this.data.materialsList
+    let newList = this.data.materialsList;
     /* 修改对应索引的name值 */
-    newList[index].name = name
+    newList[index].name = name;
     /* 更新数据 */
     this.setData({
-      materialsList: newList
-    })
+      materialsList: newList,
+    });
   },
-  handleUpdateMaterialsQuantity(e){
+  handleUpdateMaterialsQuantity(e) {
     /* 获取输入框中的值 */
-    const quantity = e.detail.value
+    const quantity = e.detail.value;
     /* 获取组件中携带的数据索引 */
-    const index = e.currentTarget.dataset.index
+    const index = e.currentTarget.dataset.index;
     /* 获取data中的材料列表数组 */
-    let newList = this.data.materialsList
+    let newList = this.data.materialsList;
     /* 修改对应索引的name值 */
-    newList[index].quantity = quantity
+    newList[index].quantity = quantity;
     /* 更新数据 */
     this.setData({
-      materialsList: newList
+      materialsList: newList,
+    });
+  },
+  /* 点击发布按钮 */
+  async handelPublish() {
+    if(this.data.title.trim()===''){
+      this.showTip('标题不能为空！')
+      return;
+    }
+    if(this.data.introd.trim()===''){
+      this.showTip('简介不能为空！')
+      return;
+    }
+    if(this.data.img.trim()===''){
+      this.showTip('请选择封面！')
+      return;
+    }
+
+    const {html:steps,text} = await this.editorCtx.getContents()
+    if(text.trim()===''){
+      this.showTip('步骤不能为空！')
+      return;
+    }
+
+
+    const params = {
+      title: this.data.title,
+      introd: this.data.introd,
+      img:this.data.img,
+      nickname:this.data.nickName,
+      materials:this.data.materialsList,
+      content:steps
+    }
+
+    const {data:response} = await http.post('/menu/publish',params)
+    if(response.code === 1000){
+      if(response.data === 'ok'){
+        /* 发布成功跳转到我的发布页面 */
+        this.showTip('发布成功')
+        wx.navigateTo({url:'/pages/mypublish/mypublish'})
+        return;
+      }
+    }
+
+    this.showTip('发布失败，请重试')
+    
+  },
+
+  /* 显示输入合法的提示 */
+  showTip(tip){
+    wx.showToast({
+      title: tip,
+      icon: 'none',
+      duration: 1000
     })
   },
 
   /* 下面是富文本编辑器需要的方法 */
   readOnlyChange() {
     this.setData({
-      readOnly: !this.data.readOnly
-    })
+      readOnly: !this.data.readOnly,
+    });
   },
   onLoad() {
-    const platform = wx.getSystemInfoSync().platform
-    const isIOS = platform === 'ios'
-    this.setData({ isIOS})
-    const that = this
-    this.updatePosition(0)
-    let keyboardHeight = 0
-    wx.onKeyboardHeightChange(res => {
-      if (res.height === keyboardHeight) return
-      const duration = res.height > 0 ? res.duration * 1000 : 0
-      keyboardHeight = res.height
+     // 从缓存中获取
+     const userInfo = wx.getStorageSync('userinfo')
+     // 对象判空不可以直接使用 === null来判断
+     if(Object.keys(userInfo) != 0){
+       // 设置用户信息 修改用户登录状态
+       this.setData({
+        nickName: userInfo.nickName
+       })
+     }else{
+       // 用户未登录 先登录
+       wx.navigateTo({
+         url: '/pages/login/login'
+       });
+     }
+
+
+
+    const platform = wx.getSystemInfoSync().platform;
+    const isIOS = platform === "ios";
+    this.setData({ isIOS });
+    const that = this;
+    this.updatePosition(0);
+    let keyboardHeight = 0;
+    wx.onKeyboardHeightChange((res) => {
+      if (res.height === keyboardHeight) return;
+      const duration = res.height > 0 ? res.duration * 1000 : 0;
+      keyboardHeight = res.height;
       setTimeout(() => {
         wx.pageScrollTo({
           scrollTop: 0,
           success() {
-            that.updatePosition(keyboardHeight)
-            that.editorCtx.scrollIntoView()
-          }
-        })
-      }, duration)
-
-    })
+            that.updatePosition(keyboardHeight);
+            that.editorCtx.scrollIntoView();
+          },
+        });
+      }, duration);
+    });
   },
   updatePosition(keyboardHeight) {
-    const toolbarHeight = 50
-    const { windowHeight, platform } = wx.getSystemInfoSync()
-    let editorHeight = keyboardHeight > 0 ? (windowHeight - keyboardHeight - toolbarHeight) : windowHeight
-    this.setData({ editorHeight, keyboardHeight })
+    const toolbarHeight = 50;
+    const { windowHeight, platform } = wx.getSystemInfoSync();
+    let editorHeight =
+      keyboardHeight > 0
+        ? windowHeight - keyboardHeight - toolbarHeight
+        : windowHeight;
+    this.setData({ editorHeight, keyboardHeight });
   },
   calNavigationBarAndStatusBar() {
-    const systemInfo = wx.getSystemInfoSync()
-    const { statusBarHeight, platform } = systemInfo
-    const isIOS = platform === 'ios'
-    const navigationBarHeight = isIOS ? 44 : 48
-    return statusBarHeight + navigationBarHeight
+    const systemInfo = wx.getSystemInfoSync();
+    const { statusBarHeight, platform } = systemInfo;
+    const isIOS = platform === "ios";
+    const navigationBarHeight = isIOS ? 44 : 48;
+    return statusBarHeight + navigationBarHeight;
   },
   onEditorReady() {
-    const that = this
-    wx.createSelectorQuery().select('#editor').context(function (res) {
-      that.editorCtx = res.context
-    }).exec()
+    const that = this;
+    wx.createSelectorQuery()
+      .select("#editor")
+      .context(function (res) {
+        that.editorCtx = res.context;
+      })
+      .exec();
   },
   blur() {
-    this.editorCtx.blur()
+    this.editorCtx.blur();
   },
   format(e) {
-    let { name, value } = e.target.dataset
-    if (!name) return
+    let { name, value } = e.target.dataset;
+    if (!name) return;
     // console.log('format', name, value)
-    this.editorCtx.format(name, value)
-
+    this.editorCtx.format(name, value);
   },
   onStatusChange(e) {
-    const formats = e.detail
-    this.setData({ formats })
+    const formats = e.detail;
+    this.setData({ formats });
   },
   insertDivider() {
     this.editorCtx.insertDivider({
       success: function () {
-        console.log('insert divider success')
-      }
-    })
+        console.log("insert divider success");
+      },
+    });
   },
   clear() {
     this.editorCtx.clear({
       success: function (res) {
-        console.log("clear success")
-      }
-    })
+        console.log("clear success");
+      },
+    });
   },
   removeFormat() {
-    this.editorCtx.removeFormat()
+    this.editorCtx.removeFormat();
   },
   insertDate() {
-    const date = new Date()
-    const formatDate = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+    const date = new Date();
+    const formatDate = `${date.getFullYear()}/${
+      date.getMonth() + 1
+    }/${date.getDate()}`;
     this.editorCtx.insertText({
-      text: formatDate
-    })
+      text: formatDate,
+    });
   },
   insertImage() {
-    const that = this
+    const that = this;
     wx.chooseImage({
       count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
+      sizeType: ["original", "compressed"],
+      sourceType: ["album", "camera"],
       success: function (res) {
         /* 上传图片到服务器 */
         wx.uploadFile({
-          url: serverIp+'/img/upload',
+          url: serverIp + "/img/upload",
           filePath: res.tempFilePaths[0],
-          name: 'pic',
+          name: "pic",
           formData: {},
-          success: (result)=>{
-            const data = JSON.parse(result.data)
+          success: (result) => {
+            const data = JSON.parse(result.data);
             /* 成功上传 */
-            if(data.code === 1000){
+            if (data.code === 1000) {
               /* 获取该图片的url */
-              const url = data.data
+              const url = data.data;
               that.editorCtx.insertImage({
                 /* 替换imgage标签的src属性 */
                 src: url,
                 data: {
-                  id: 'abcd',
-                  role: 'god'
+                  id: "abcd",
+                  role: "god",
                 },
-                width: '80%',
+                width: "80%",
                 success: function () {
                   /* 提示用户上传成功 */
                   wx.showToast({
-                    title: '图片上传成功',
-                    icon: 'success',
-                    duration: 1000
-                  })
-                }
-              })
+                    title: "图片上传成功",
+                    icon: "success",
+                    duration: 1000,
+                  });
+                },
+              });
             }
           },
-          fail: ()=>{
+          fail: () => {
             wx.showToast({
-              title: '图片上传失败',
-              icon: 'fail',
-              duration: 1000
-            })
+              title: "图片上传失败",
+              icon: "none",
+              duration: 1000,
+            });
           },
-          complete: ()=>{}
+          complete: () => {},
         });
-       
-        
-      }
-    })
-  }
-})
+      },
+    });
+  },
+});
