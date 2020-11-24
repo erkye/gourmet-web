0e3b414564455fa957625ba4ea8fcc176c84cf89
lifazhan <lifazhan233@gmail.com>
2020-11-24 10:07:56 +0800

    end

--- resquent/http.js
+++ resquent/http.js
@@ -5,8 +5,8 @@ axios.defaults.adapter = mpAdapter
 /* 真机调试使用 */
   /* baseURL: 'http://243769da24.wicp.vip/api' */
   /* 生产环境使用 */
-  /* baseURL: 'http://150.158.174.106:3000/api' */
-const serverIp = "http://150.158.174.106:3000/api"
+  /* baseURL: 'http://:3000/api' */
+const serverIp = "http://localhost:3000/api"
 // 构建axios实例
 const http = axios.create({
   /* 本地测试环境使用 */
